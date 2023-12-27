1adbdbe065fa955c4375b48d541277e385908db9
mutuku1998 <martinmutuku1998@gmail.com>
2023-12-27 11:28:57 +0300

    m

---
+++ laravel-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\CustomerCollection;
+ use App\Services\V1\CustomerQuery;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request);
+
+        if(count($queryItems)==0){
+
+            return new CustomerCollection (Customer::paginate());
+
+        }else{
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Customer $customer)
+    {
+      return new CustomerResource($customer);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ laravel-api/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//api/v1
+
+Route::group(['prefix'=>'v1', 'namespace' =>'App\Http\Controllers\Api\v1'], function(){
+
+    Route::apiResource('customers',CustomerController::class);
+    Route::apiResource('invoices',InvoiceController::class);
+
+
+});
