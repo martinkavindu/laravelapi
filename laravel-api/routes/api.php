0e8aaab7f21367343e6d8220fec30ee05043b1b2
Mutuku1998 <martinmutuku1998@gmail.com>
2024-04-08 22:32:00 +0300

    m

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//endpoints
+Route::group(['prefix' => 'v1','namespace' => 'App\Http\Controllers\Api\V1'],function(){
+
+Route::apiResource('customers',CustomerController::class);
+Route::apiResource('invoices',InvoiceController::class);
+
+});
