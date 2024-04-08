27ff25fd880be3a59d399295a8e09b41c9b7d296
Mutuku1998 <martinmutuku1998@gmail.com>
2024-04-08 16:58:02 +0300

    m

--- laravel-api/database/seeders/DatabaseSeeder.php
+++ laravel-api/database/seeders/DatabaseSeeder.php
@@ -10,13 +10,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
         // \App\Models\User::factory(10)->create();
 
-        $this->call([
-            CustomerSeeder::class
-        ]);
-
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\CustomerController;
-use App\Http\Controllers\Api\V1\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +16,4 @@ use App\Http\Controllers\Api\V1\InvoiceController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-//api/v1
-
-Route::group(['prefix'=>'v1', 'namespace' =>'App\Http\Controllers\Api\v1'], function(){
-
-    Route::apiResource('customers',CustomerController::class);
-    Route::apiResource('invoices',InvoiceController::class);
-
-
 });
